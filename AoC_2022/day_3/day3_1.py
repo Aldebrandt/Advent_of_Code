764dc085f4e8ba5158dfb8e0c1d267e27470031f
matthias-ulrichkarras <matthias@hdkarras.de>
2022-12-03 07:03:14 +0100

    feat: day3_1.py - add my solution to part 1

---
+++ AoC_2022/day_3/day3_1.py
@@ -0,0 +1,10 @@
+from day3_data import rucksacks
+import string
+
+rucksack_list = [rucksack for rucksack in rucksacks.split("\n")]
+rucksack_list_splitted = [(rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]) for rucksack in rucksack_list]
+rucksack_list_common_i = ["".join(set(rsl[0]).intersection(rsl[1])) for rsl in rucksack_list_splitted]
+letter_weight = [(j, i) for i, j in enumerate(string.ascii_letters, start=1)]
+test = [lw[1] for lw in letter_weight for common_letter in rucksack_list_common_i if lw[0] == common_letter]
+
+print(sum(test))
